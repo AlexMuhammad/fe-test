866eb62c1ddc9b8769b0800b186b8efb10a3b32e
AlexMuhammad <alexmuhammad@students.amikom.ac.id>
2023-08-05 17:18:48 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/molecules/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <h1 className='text-red-300'>tes</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/molecules/Navbar.jsx
@@ -0,0 +1,7 @@
+const Navbar = () => {
+    return (
+        <div className="bg-primary-1">Navbar</div>
+    )
+}
+
+export default Navbar
